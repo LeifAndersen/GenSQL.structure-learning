9b0e4b360067a2046a39b3462f67f28549a7ef39
Leif Andersen <leif@leifandersen.pl>
2025-08-05 13:02:51 -0400

    Get the vm working in a docker container:
    1. Does NOT use loom. Loom seems to be broken at the moment.
    
    2. Requires the model name to be '0.edn`, not `sample.0.edn`.
    
    3. Is only tested in one container.

--- scripts/linear_stats.py
+++ scripts/linear_stats.py
@@ -8,11 +8,9 @@ import pandas
 import sys
 import numpy as np
 import scipy.stats as stats
-import warnings
 from collections import defaultdict
 
 # Monkey patching this to work around https://github.com/scipy/scipy/pull/7838
-infinite_F = 1e10  # large value to use for writing infinity to json
 
 if not hasattr(stats, "frechet_r"):
     stats.frechet_r = stats.weibull_max
@@ -21,77 +19,61 @@ if not hasattr(stats, "frechet_l"):
     stats.frechet_l = stats.weibull_min
 
 
-def placeholder_stats(stattypes):
-    match stattypes:
-        case ["numerical", "numerical"]:
-            return {
-                "slope": 0.0,
-                "intercept": 0.0,
-                "r-value": 0.0,
-                "p-value": 1.0,
-            }
-        case ["nominal", "nominal"]:
-            return {"chi2": 0, "p-value": 1, "dof": 0}
-        case ["nominal", "numerical"] | ["numerical", "nominal"]:
-            return {"F": 0, "p-value": 1}
-        case _:
-            raise ValueError(stattypes)
-
-
-def compute_stats(stattypes, xs, ys):
-    match stattypes:
-        case ["numerical", "numerical"]:
-            return lin_reg(xs, ys)
-        case ["nominal", "nominal"]:
-            return chi_squared(xs, ys)
-        case ["nominal", "numerical"]:
-            return anova(xs, ys)
-        case ["numerical", "nominal"]:
-            return anova(ys, xs)
-        case _:
-            raise ValueError(stattypes)
-
-
 def lin_reg(xs, ys):
     """Compute linear regression if we have data on both xs and ys"""
-    r = stats.linregress(xs, ys)
+    if len(xs) > 0:  # Check whether nan mask return data
+        r = stats.linregress(xs, ys)
+        # Check if stats.linregress ran into numerical problems.
+        if not (np.isnan(r.slope) or np.isnan(r.intercept)):
+            return {
+                "slope": r.slope,
+                "intercept": r.intercept,
+                "r-value": r.rvalue,
+                "p-value": r.pvalue,
+            }
+    # Else: return a placeholder that works with subsequent dvc stages.
     return {
-        "slope": r.slope,
-        "intercept": r.intercept,
-        "r-value": r.rvalue,
-        "p-value": r.pvalue,
+        "slope": 0.0,
+        "intercept": 0.0,
+        "r-value": 0.0,
+        "p-value": 1.0,
     }
 
 
 def chi_squared(xs, ys):
     """Compute chi-square statistic"""
     contingency = pandas.crosstab(xs, ys)
-    chi2, p, dof, expected = stats.chi2_contingency(contingency)
+    try:
+        chi2, p, dof, expected = stats.chi2_contingency(contingency)
+    except ValueError: ### XXX This is probably wrong!
+        chi2, p, dof, expected = 0, 0, 0, 0
     return {"chi2": chi2, "p-value": p, "dof": dof}
 
 
-def anova(c1, c2):
+def not_null(v):
+    if isinstance(v, str):
+        return True
+    elif v is None:
+        return False
+    elif np.isnan(v):
+        return False
+    return True
+
+
+def anova(df, c1, c2):
     """Compute One-way ANOVA"""
-    samples = defaultdict(list)
-    for categorical_value, continuous_value in zip(c1, c2):
-        samples[categorical_value].append(continuous_value)
-
-    if len(samples) > 1:
-        warnings.filterwarnings("error")
-        # ANOVA warnings from https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.f_oneway.html
-        try:
-            F, p = stats.f_oneway(*samples.values())
-        except stats.ConstantInputWarning:
-            # Raised if all values within each of the input arrays are identical.
-            # In this case the F statistic is either infinite or isn’t defined, so np.inf or
-            # np.nan is returned.
-            return {"F": infinite_F, "p-value": 0}
-        except stats.DegenerateDataWarning:
-            # Raised if the length of any input array is 0, or if all the input arrays have length 1.
-            # np.nan is returned for the F statistic and the p-value in these cases.
-            return {"F": 0, "p-value": 1}
-
-    return {"F": 0, "p-value": 1}
+    samples = []
+    for categorical_value in df[c1].unique():
+        # Check whether this is actually a string category
+        if not_null(categorical_value):
+            samples.append(
+                df[(df[c1] == categorical_value) & (~df[c2].isnull())][c2].values
+            )
+    F, p = stats.f_oneway(*samples)
+    return {
+        "F": F,
+        "p-value": p,
+    }
 
 
 def main():
@@ -119,24 +101,39 @@ def main():
 
     schema = edn_format.loads(args.schema.read(), write_ply_tables=False)
 
-    pairs = itertools.permutations(df.columns, 2)
+    pairs = itertools.combinations(df.columns, 2)
 
     results = defaultdict(dict)
 
     for c1, c2 in pairs:
-        stattypes = [schema[c1].name, schema[c2].name]
+        stattypes = {schema[c1].name, schema[c2].name}
+        c1_vals = df[c1].values
+        c2_vals = df[c2].values
         if "ignore" in stattypes:
-            continue
-        pair_df = df[[c1, c2]].dropna()
-
-        if len(pair_df) <= 1:
-            results[c1][c2] = placeholder_stats(stattypes)
-
+            pass
         else:
-            c1_vals = pair_df[c1].values
-            c2_vals = pair_df[c2].values
-
-            results[c1][c2] = compute_stats(stattypes, c1_vals, c2_vals)
+            if stattypes == {"numerical"}:
+                nan_mask = ~np.isnan(c1_vals) & ~np.isnan(c2_vals)
+                # Store c1, c2 regression.
+                r_items_1 = lin_reg(c1_vals[nan_mask], c2_vals[nan_mask])
+                results[c1][c2] = r_items_1
+                # Slope and intercept have different values in the inverse
+                # direction.
+                r_items_2 = lin_reg(c2_vals[nan_mask], c1_vals[nan_mask])
+                results[c2][c1] = r_items_2
+            elif stattypes == {"nominal"}:
+                chi_s_items = chi_squared(c1_vals, c2_vals)
+                results[c1][c2] = chi_s_items
+                results[c2][c1] = chi_s_items
+            elif stattypes == {"nominal", "numerical"}:
+                if schema[c1].name == "nominal":
+                    anova_items = anova(df, c1, c2)
+                else:
+                    anova_items = anova(df, c2, c1)
+                results[c1][c2] = anova_items
+                results[c2][c1] = anova_items
+            else:
+                raise ValueError(stattypes)
 
     json.dump(results, args.output)
 
