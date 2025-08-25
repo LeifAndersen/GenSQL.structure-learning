4a05c71ab3e7ae7e91071e5bbcdbbdcf8046f93c
Leif Andersen <leif@leifandersen.pl>
2025-08-25 00:13:34 -0400

    Validate script had hidden dependency to data.csv.
    I logged it, and switched the dependency to pivoted.csv

--- bin/validate.sh
+++ bin/validate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if INVALID_COLUMNS=$(xsv headers -j data/data.csv | grep -v -E "^[0-9a-zA-Z_\-]+$")
+if INVALID_COLUMNS=$(xsv headers -j data/pivoted.csv | grep -v -E "^[0-9a-zA-Z_\-]+$")
 then
     printf 'Column names may only include alphanumeric characters, dashes, or underscores.\n\n'
     printf 'Invalid column names:\n\n%s\n' "$INVALID_COLUMNS"
